3041077b271200ba7a243d8a3ce5d6d167dc7695
Girish <girish@post.com>
2017-05-05 11:02:08 -0700

    View image using pyplot

--- lesson3/working_with_image.py
+++ lesson3/working_with_image.py
@@ -1,7 +1,10 @@
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 filename = dir_path + "/MarshOrchid.jpg"
 image = mpimg.imread(filename)
 print(image.shape)
+plt.imshow(image)
+plt.show()
