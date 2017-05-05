7fbd2d181e0e8d11484caaf620536774da236ea6
Girish <girish@post.com>
2017-05-05 09:47:22 -0700

    Exercise 1 and 2

---
+++ lesson2/ex2.py
@@ -0,0 +1,17 @@
+'''Generate a NumPy array of 10,000 random numbers (called x) 
+and create a Variable storing the equation y=5x2−3x+15'''
+import tensorflow as tf
+import numpy as np
+
+data = np.random.randint(1000, size=10000)
+
+x = tf.constant(data, name='x')
+y = tf.Variable((5 * x * x) -( 3 * x) + 15, name='y')
+
+model = tf.global_variables_initializer()
+
+with tf.Session() as session:
+    session.run(model)
+    runresult = session.run(y)
+    print("Length of result: %d"%(len(runresult)))
+    print(result)
