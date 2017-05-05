3979b48beace5f627ade472cd440c4e8c33ffef8
Girish <girish@post.com>
2017-05-05 10:24:03 -0700

    ex4 different interpretaion, summing random and then average

--- lesson2/ex4.py
+++ lesson2/ex4.py
@@ -8,12 +8,13 @@ import numpy as np
 
 y = tf.Variable(np.random.randint(1000), name='y')
 avg = tf.Variable(0., name='avg')
+total = tf.Variable(0., name='total')
 model = tf.global_variables_initializer()
 
 with tf.Session() as session:
     for idx in range(1000):
         session.run(model)
         y = np.random.randint(1000)
-        avg = (avg + y)/(idx+1.0)
-        print(session.run(avg))
-        
+        total += y
+        avg = (total)/(idx+1.0)
+        print(idx,session.run(avg))
